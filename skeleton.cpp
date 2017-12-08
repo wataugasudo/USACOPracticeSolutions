a12163df2ffae5325c933a749f1c94fb26c80977
Benjamin Broce <benbroce2@gmail.com>
2017-12-08 09:01:54 -0500

    Create skeleton.cpp

---
+++ skeleton.cpp
@@ -0,0 +1,18 @@
+//Solves []
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+int main() {
+    freopen("FILENAME.in", "r", stdin);
+    freopen("FILENAME.out", "w", stdout);
+    
+    //cin>>INPUTVAR;
+    //cout<<OUTPUTVAR;
+    
+    return 0;
+}
