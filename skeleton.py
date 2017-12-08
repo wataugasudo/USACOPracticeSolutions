4f658e944d78171b59652c0a197fe32846b7c9b0
Benjamin Broce <benbroce2@gmail.com>
2017-12-08 06:36:30 -0500

    Create skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,14 @@
+#Solves []
+
+#Safely read list of String lines from input file
+with open("FILENAME.in", "r") as inFile:
+    text = inFile.readlines()
+
+#Make a String line element of text[] into a list of integers
+#variableName = list(map(int, text[0].split()))
+
+#YOUR CODE HERE
+
+#Safely (create &) write to output file
+with open("FILENAME.out", "w") as outFile:
+    outFile.write()
